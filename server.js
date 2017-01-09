f22cb10a7af8ebd3c88b0dbcc7bcc6da3f032cab
Joe Doyle <jdoyle@nodesource.com>
2017-01-08 20:09:17 -0800

    small updates

--- server.js
+++ server.js
@@ -7,65 +7,64 @@ const Inert = require('inert');
 
 // Create a server with a host and port
 const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: path.join(__dirname, 'public')
-            }
-        }
+  connections: {
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, 'public')
+      }
     }
+  }
 });
 
-server.connection({ 
-  host: 'localhost', 
-  port: 8000 
+server.connection({
+  host: 'localhost',
+  port: 8000
 });
 
 server.register(Inert, () => {});
 
 // Add the route
 server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            defaultExtension: 'html',
-            index: true
-        }
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      defaultExtension: 'html',
+      index: true
     }
+  }
 });
-
 
 // Start the server
 server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
-        }
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
     }
+  }
 }, (err) => {
 
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+
     if (err) {
-        throw err; // something bad happened loading the plugin
+      throw err;
     }
-
-    server.start((err) => {
-
-        if (err) {
-            throw err;
-        }
-        server.log('info', 'Crocker Minecraft Club Server running at: ' + server.info.uri);
-    });
+    server.log('info', 'Crocker Minecraft Club Server running at: ' + server.info.uri);
+  });
 });
